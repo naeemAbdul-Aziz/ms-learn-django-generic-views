83d2d17d20c540ea553ad95ecd54b0850877b3f1
unknown <naeemabdulaziz202@gmail.com>
2024-08-06 13:07:02 +0000

    implement generic views

---
+++ starter/dog_shelters/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.shelter_list, name='shelter_list'),
+    path('shelter/<int:pk>', views.shelter_detail, name='shelter_detail'),
+
+    # TODO: Register detail view
+    path('dog/<int:pk>', views.DogDetailView.as_view(), name='dog_detail'),
+
+    # TODO: Register create view
+
+
+]
